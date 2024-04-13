575b1d79121a1223d0a457bc3c829893ae131446
João Carlos <146602405+JoaoCalrosAparecido@users.noreply.github.com>
2024-04-13 12:45:10 -0300

    comit

--- app/public/barra_de_pesquisa.js
+++ app/public/barra_de_pesquisa.js
@@ -1,9 +1,8 @@
 const searchIcon = document.getElementById('searchIcon');
 const searchInput = document.getElementById('searchInput');
-let timeoutId;
 
-searchIcon.addEventListener('mouseenter', function() {
-  searchInput.classList.add('show');
+searchIcon.addEventListener('click', function() {
+  searchInput.classList.toggle('show');
 });
 
 searchInput.addEventListener('click', function(event) {
@@ -11,17 +10,9 @@ searchInput.addEventListener('click', function(event) {
 });
 
 document.addEventListener('click', function(event) {
-  clearTimeout(timeoutId); // Limpa o timer ao clicar em qualquer parte do documento
   const isClickInsideSearchContainer = searchIcon.contains(event.target) || searchInput.contains(event.target);
   
   if (!isClickInsideSearchContainer) {
     searchInput.classList.remove('show');
   }
-});
-
-// Define um temporizador para esconder a barra de pesquisa após 2 segundos
-searchInput.addEventListener('mouseleave', function() {
-  timeoutId = setTimeout(function() {
-    searchInput.classList.remove('show');
-  }, 500);
 });
