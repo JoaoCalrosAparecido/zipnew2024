a85b3cc918caabdfe0d923f6c866c5c9029270ef
Lazullyz <arthurmendz97@gmail.com>
2024-04-26 08:57:10 -0300

    gloria

--- app/public/bolsa_preta_classica/denunciar/script.js
+++ app/public/bolsa_preta_classica/denunciar/script.js
@@ -1,13 +1,13 @@
-document.getElementById("send").addEventListener("click", function() {
+document.getElementById("denunciarBtn").addEventListener("click", function() {
     // Mostra "Denunciando" no botão
-    document.getElementById("send").innerText = "Denunciando...";
+    document.getElementById("denunciarBtn").innerText = "Denunciando...";
   
     // Simula uma denúncia sendo feita (tempo de espera de 2 segundos)
     setTimeout(function() {
       // Após 2 segundos, mostra a mensagem de sucesso e altera o texto do botão
       document.getElementById("mensagem").innerText = "Sua denúncia foi realizada com sucesso.";
       document.getElementById("mensagem").style.display = "block";
-      document.getElementById("send").innerText = "Denunciado";
+      document.getElementById("denunciarBtn").innerText = "Denunciado";
     }, 2000);
   });
   
