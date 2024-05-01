fa7b65377f64e1104c34c381ba0717e0e5db1ac8
João Carlos <146602405+JoaoCalrosAparecido@users.noreply.github.com>
2024-04-30 22:35:33 -0300

    stories recentes

--- app/public/new_in_slide.js
+++ app/public/new_in_slide.js
@@ -1,3 +1,4 @@
+/*antigo
 const carouselSlide = document.querySelector('.carousel-slide');
 const slides = document.querySelectorAll('.slide');
 const paginationItems = document.querySelectorAll('.pagination-item');
@@ -19,4 +20,27 @@ setInterval(() => {
         item.classList.remove('active');
     });
     paginationItems[counter].classList.add('active');
+}, 2000);*/
+
+const carouselSlide1 = document.querySelector('.carousel-slide');
+const slides1 = document.querySelectorAll('.slide');
+const paginationItems1 = document.querySelectorAll('.pagination-item');
+
+let counter1 = 0;
+const slideWidth1 = slides1[0].clientWidth;
+
+carouselSlide1.style.transform = `translateX(${-slideWidth1 * counter1}px)`;
+
+setInterval(() => {
+    counter1++;
+    if (counter1 >= slides1.length) {
+        counter1 = 0;
+    }
+    carouselSlide1.style.transition = 'transform 0.2s ease-in-out';
+    carouselSlide1.style.transform = `translateX(${-slideWidth1 * counter1}px)`;
+
+    paginationItems1.forEach(item => {
+        item.classList.remove('active');
+    });
+    paginationItems1[counter1].classList.add('active');
 }, 2000);
--- app/public/scroll_cell.js
+++ app/public/scroll_cell.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
       const rect = square.getBoundingClientRect();
-      if (rect.top >= 8 && rect.bottom <= window.innerHeight) {
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
         square.style.transform = 'scale(1)';
       } else {
         square.style.transform = 'scale(0.8)';
