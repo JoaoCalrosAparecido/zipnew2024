fd8762a0009f7788487a7b6e8348f45f17122be1
João Carlos <146602405+JoaoCalrosAparecido@users.noreply.github.com>
2024-05-06 20:36:12 -0300

    comit

---
+++ app/public/js/rolagem_icones.js
@@ -0,0 +1,27 @@
+/*barra de pesquisa*/
+window.addEventListener('scroll', function() {
+    var icon1 = document.getElementById('icon1_principal');
+    var icon2 = document.getElementById('icon2_principal');
+    
+    if (window.scrollY > 300) {
+        icon1.style.display = 'block';
+        icon2.style.display = 'none';
+    } else {
+        icon1.style.display = 'none';
+        icon2.style.display = 'block';
+    }
+});
+
+/*favorito*/
+window.addEventListener('scroll', function() {
+    var icon1 = document.getElementById('lordicon');
+    var icon2 = document.getElementById('lordicon2');
+    
+    if (window.scrollY > 300) {
+        icon1.style.display = 'block';
+        icon2.style.display = 'none';
+    } else {
+        icon1.style.display = 'none';
+        icon2.style.display = 'block';
+    }
+});
