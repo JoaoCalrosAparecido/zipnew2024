53d9fa681bf5d6f529ea4d1e26ea7946194d1ff8
João Carlos <146602405+JoaoCalrosAparecido@users.noreply.github.com>
2024-06-04 07:26:02 -0300

    comit

--- app/public/rolagem_cell_icones.js
+++ app/public/rolagem_cell_icones.js
@@ -1,10 +1,13 @@
 window.addEventListener('scroll', function() {
     var icon1 = document.getElementById('icon1');
+    var icon2 = document.getElementById('icon2');
     
     if (window.scrollY > 300) {
         icon1.style.display = 'block';
+        icon2.style.display = 'none';
     } else {
         icon1.style.display = 'none';
+        icon2.style.display = 'block';
     }
 });
 
