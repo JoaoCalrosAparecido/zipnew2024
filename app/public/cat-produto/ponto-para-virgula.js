8a7d472c6f3dcf82c9934301a6af49b4fe953690
Joao Carlos <joaocarlosammocale@gmail.com>
2024-10-06 00:56:23 -0300

    rodape do adm denuncias e famosos

--- app/public/cat-produto/ponto-para-virgula.js
+++ app/public/cat-produto/ponto-para-virgula.js
@@ -1,12 +1,30 @@
 window.onload = function() {
-    var precos = document.querySelectorAll('.product-price');
+    // Para os elementos com a classe 'product-price'
+    var precos00 = document.querySelectorAll('.product-price');
+    precos00.forEach(function(p) {
+        p.innerHTML = p.innerHTML.replace(/\./g, ',');
+    });
+
+    // Para o elemento com o ID 'parte_preço_bolsa_Preta'
+    var precos0 = document.getElementById('parte_preço_bolsa_Preta');
+    if (precos0) { // Verifica se o elemento existe
+        precos0.innerHTML = precos0.innerHTML.replace(/\./g, ',');
+    }
+
+    // Para o elemento com o ID 'parte_escrita_subtotal'
+    var preco1 = document.getElementById('parte_escrita_subtotal');
+    if (preco1) { // Verifica se o elemento existe
+        preco1.innerHTML = preco1.innerHTML.replace(/\./g, ',');
+    }
+
+    // Para os elementos com a classe 'preco-prod'
+    var precos = document.querySelectorAll('.preco-prod');
     precos.forEach(function(p) {
         p.innerHTML = p.innerHTML.replace(/\./g, ',');
     });
-};
-window.onload = function() {
-    var preco = document.getElementById('parte_preço_bolsa_Preta');
-    preco.forEach(function(p) {
+
+    var precos = document.querySelectorAll('.parte_do_total');
+    precos.forEach(function(p) {
         p.innerHTML = p.innerHTML.replace(/\./g, ',');
     });
 };
