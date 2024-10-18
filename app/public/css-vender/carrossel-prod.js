b30ce1c45ded337f92695b5d13c74e105fd882b1
sophi-hub <alexconner470@gmail.com>
2024-10-17 22:05:26 -0300

    corrigindo efeito
    carrossel bugado2

---
+++ app/public/css-vender/carrossel-prod.js
@@ -0,0 +1,10 @@
+var swiper2 = new Swiper2(".mySwiper2", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    scrollbar: {
+      el: ".swiper-scrollbar2",
+      hide: true,
+    },
+  });
--- app/public/css-vender/fadeIn.js
+++ app/public/css-vender/fadeIn.js
@@ -1,15 +1,15 @@
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-      if (entry.intersectionRatio >= 1) {
+      if (entry.intersectionRatio >= 0.5) { // 50% do elemento visível
           if (entry.target.classList.contains('init-hidden--direita')) {
               entry.target.classList.add('animate__fadeInRight');
           } else if (entry.target.classList.contains('init-hidden')) {
               entry.target.classList.add('animate__fadeIn');
-          } 
+          }
       }
   });
 }, {
-  threshold: [0, .5, 1]
+  threshold: [0, 0.5, 1] // Ajustando o threshold para diferentes níveis de visibilidade
 });
 
 document.querySelectorAll('.init-hidden, .init-hidden--direita').forEach(element => {
