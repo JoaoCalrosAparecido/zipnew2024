2d0711ebd765cc1922dcc49e10dec894240f2250
sophi-hub <alexconner470@gmail.com>
2024-10-26 15:53:58 -0300

    wishlist

--- app/public/wishlist/js/slide_wishlist.js
+++ app/public/wishlist/js/slide_wishlist.js
@@ -1,13 +1,8 @@
-var swiper = new Swiper(".mySwiper1", {
+var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
-    spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination1",
-        clickable: true,
     },
 });
